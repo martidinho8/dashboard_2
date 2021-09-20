899864e193bf74de37b62ce5f7d20429475d93ad
unknown <martijnreinders@live.nl>
2021-09-20 18:08:30 +0200

    First version

---
+++ src/views/Contact.vue
@@ -0,0 +1,7 @@
+<template>
+<v-app>
+  <v-content class="contact">
+    <h1>This is an about page</h1>
+  </v-content>
+  </v-app>
+</template>
---
+++ src/views/modules.vue
@@ -0,0 +1,51 @@
+<template>
+<v-app>
+<v-content class="row">
+      <div class="col-md-4 col-lg4" v-for="(data,index) in products" :key="index">
+        <img :src="data.image" class="img-fluid">
+         <h3>{{data.productTitle}}</h3>
+      </div>
+    </v-content>
+    </v-app>
+</template>
+
+<script>
+export default {
+  name: 'modules',
+  data () {
+    return {
+      title: 'Home',
+      products:[
+      {
+        productTitle:"ABCN",
+        productId:1
+      },
+      {
+        productTitle:"KARMA",
+        productId:2
+      },
+      {
+        productTitle:"Tino",
+        productId:3
+      },
+      {
+        productTitle:"EFG",
+        productId:4
+      },
+      {
+        productTitle:"MLI",
+        productId:5
+      },
+      {
+        productTitle:"Banans",
+        productId:6
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+ 
+</style>
